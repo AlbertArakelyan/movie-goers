681b879558961a54aa896686500336f50887f01d
ASahak <arthur.sahakyan96@gmail.com>
2023-07-18 20:23:07 +0400

    chakra setup

---
+++ src/components/Pages/Home/Cards/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {ContainerPropTypes} from './types';
+import View from './view';
+
+const CardsContainer: React.FC<ContainerPropTypes> = () => {
+
+  return <View/>
+}
+export default React.memo(CardsContainer);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import theme from '@/styles/theme';
+import {ChakraProvider} from '@chakra-ui/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+  return <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
